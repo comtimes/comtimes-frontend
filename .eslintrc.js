f6644f0dd327ddaced59ba6d932d6a3728a6e97e
jiwon_lala <s26788761@naver.com>
2021-08-16 00:29:32 +0900

    Update eslint and prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,13 +25,19 @@ module.exports = {
       2,
       { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-    "linebreak-style": 0,
+    // "prettier/prettier": [
+    //   "error",
+    //   {
+    //     endOfLine: "auto",
+    //   },
+    // ],
+    "prettier/prettier": 0, // trailing comma 해결하기 위해 추가
+    // "import/extensions": 0,
+    // "import/no-unresolved": 0,
+    // "import/no-extraneous-dependencies": 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    // "no-shadow": 0,
+    // "react/prop-types": 0,
+    // "jsx-a11y/no-noninteractive-element-interactions": 0,
     // 추가적인 사항들은 문서 상단 링크 참고!
   },
   settings: {

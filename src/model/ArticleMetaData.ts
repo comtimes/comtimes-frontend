af9a46ec9ee16bf1a0486c06adc4924248dd00ad
zwonlala <s26788761@naver.com>
2021-11-26 21:57:14 +0900

    #8 기사의 '사진 출처', '기사 취재 및 작성', '사진 촬영', '편집' 정보를 모두 가지고 있는 타입인 ArticleMetaData 타입 추가

---
+++ src/model/ArticleMetaData.ts
@@ -0,0 +1,16 @@
+import ReporterInfo from "./ReporterInfo"
+
+type ArticleEditorMetaData = {
+    'MEDIA_MANAGER'?: ReporterInfo;
+    'EDITTING_MANAGER'?: ReporterInfo;
+    'DIRECTOR': ReporterInfo;
+}
+
+type ArticleMetaData = {
+  'SOURCE_OF_PHOTO'?: ReporterInfo[];
+  'WRITTER': ReporterInfo[];
+  'PHOTOGRAPHER'?: ReporterInfo[];
+  'EDITOR': ArticleEditorMetaData;
+};
+
+export default ArticleMetaData;

e2491e5ec80d80e9281d9b25bf5a8fd8ef42292c
zwonlala <s26788761@naver.com>
2022-02-26 21:01:10 +0900

    #9 Update <ArticleDetailView /> 컴포넌트
    작업완료된 api 반영하여 기사 상세보기 뷰 컴포넌트 <ArticleDetailView /> 수정

--- src/components/ArticleDetailView.tsx
+++ src/components/ArticleDetailView.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { ReactElement, useEffect, useState } from "react";
+import ApiUtil from "../util/ApiUtil";
 import Article from "../model/Article";
 import MarkdownView from "./MarkdownView";
 
@@ -20,17 +21,23 @@ export default function ArticleDetailView(props: ArticleDetailViewProps): ReactE
                 setArticle(undefined);
                 setLoading(true);
 
-                // const response: any = await axios.get(`~~~~/${articleId}`);
-                const response: any = {};
+                const response: any = await axios.get(`${ApiUtil.GET_NEWS_DETAIL_BY_ID}/${articleId}`);
+                const { id, postTitle, postSubtitile, postImage, postHtml, postPublishedAt, author, viewCount } = response.data.data;
 
-                // setArticle(Article.createFromObj(response.data.data.article));
                 setArticle(Article.createFromObj({
-                    id: 1,
-                    title: '안녕하세요우. 기사 MOCK 데이터 입니다',
-                    subtitle: '부제목입네다',
+                    id,
+                    title: postTitle,
+                    subtitle: postSubtitile,
+                    publishDate: new Date(postPublishedAt),
+                    author: author.name,
+                    content: postHtml,
+                    thumbnail: postImage,
+                    likes: 0,
+                    views: viewCount
                 }));
             } catch (e: any) {
                 setError(e);
+                console.error(e);
             }
 
             setLoading(false);
@@ -43,11 +50,9 @@ export default function ArticleDetailView(props: ArticleDetailViewProps): ReactE
     if (error) return <div>에러가 발생했습니다</div>;
     if (!article) return <>null</>;
 
-    const tempMdString = "# Insertion Sort Pseudocode\n\n개념 : **이미 정렬되어 있는 임시 배열?에** 있고, 현재 원소의 적절한 위치에 **`삽입`** 하는 정렬\n제자리 정렬 중 하나\n\n>`제자리 정렬(in-place sorting)`\ninput으로 전달받은 정렬할 배열 외의 추가 메모리를 요구하지 않는 정렬\n## 강의 예제 코드\n![스크린샷 2022-02-08 오후 10 42 11](https://user-images.githubusercontent.com/13375734/152999145-8c2f90ef-4bbd-4b14-85c6-550d72596c44.jpg)\n\n#### 개선해야 할 점\n- 변수명을 minValIdx 보다는 lowest 가 더 적절한 것 같다\n- swap 함수를 항상 호출하는 것이 아닌, i와 minValIdx 값이 다를때만 swap 함수를 호출하자\n";
-
     return (
-        <div className="wrapper" style={{position: 'relative', margin: '0 auto', padding: '50px 0 0 150px', maxWidth:'1000px'}}>
-            <MarkdownView mdString={tempMdString} />
+        <div className="wrapper" style={{position: 'relative', margin: '0 auto', padding: '50px 0 150px 0', maxWidth:'1000px'}}>
+            <MarkdownView mdString={article.content} />
         </div>
     );
 }
---
+++ src/util/ApiUtil.ts
@@ -0,0 +1,5 @@
+
+export default class ApiUtil {
+    // http://54.180.87.17:8080/api/v1/news/1074
+    static readonly GET_NEWS_DETAIL_BY_ID: string = `/api/v2/post`;
+}

f1bd13dda5c5933fbd14be76aae51c670b7b7bb9
zwonlala <s26788761@naver.com>
2022-03-01 20:31:11 +0900

    #3 Article Model의 값 중 현재 API 상에서 optional 하게 넘겨주는 값들 optional 하게 임시 처리
    임시 처리로 추후 subtitle, publishDate, likes 는 항상 보내주게 API 수정될 예정

--- src/model/Article.ts
+++ src/model/Article.ts
@@ -6,14 +6,13 @@ export default class Article {
   constructor(
     public id: number,
     public title: string,
-    public subtitle: string,
-    public publishDate: Date,
     public author: string, // todo: 일단은 string으로 타입 하는데, 나중엔 User 클래스로 처리해야 함
     public content: string,
     public thumbnail: string,
-    public likes: number,
-    public views: number
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    public views: number,
+    public subtitle?: string,
+    public publishDate?: Date,
+    public likes?: number
   ) {}
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any

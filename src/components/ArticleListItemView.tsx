38a7931d785aeb9e2496f498867437d58826a1c9
zwonlala <s26788761@naver.com>
2022-03-01 20:36:32 +0900

    #3 feat: <ArticleListItemView /> 썸네일 이미지 등 추가된 구조로 수정
    추후 페이지 네이션 및 라우팅 등 처리 후에 해당 <ArticleListItemView /> 클릭시 해당 기사 상세보기 뷰로 이동하는 로직 추가되어야 함.
    
    추후 API 수정 후 기사 발행 날짜 및 조회수 관련 ui 개선/추가되어야 함.

--- src/components/ArticleListItemView.tsx
+++ src/components/ArticleListItemView.tsx
@@ -8,16 +8,60 @@ interface ArticleListItemViewProps {
 function ArticleListItemView(props: ArticleListItemViewProps): ReactElement {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     // eslint-disable-next-line react/destructuring-assignment
-    const { title, publishDate, author } = props.article;
+    const { title, author, content, thumbnail } = props.article;
     return (
-        <div>
-            {/* 아래 부분 '섹션'에 대한 정보가 나와야 하나, 아직 백엔드와 해당 데이터에 대한 고민을 하지 못해 일단 비워놓음  */}
-            {/* <div>{props.section} </div> */}
-            <div className='title'>{title}</div>
-            <div className='author'>{author}</div>
-            {/* todo: 아래 발행일자 부분 일단은 toString() 처리하고 넘어가는데, 나중엔 DateUtil 만들어서 함수 만들어야 함 */}
-            {/* todo: postman 결과가 publishDate 객체가 아닌 created_at 문자열로 나오는 것 같다 -> 수정해야할 듯 */}
-            {/* <div className='publish_date'>{publishDate.toString()}</div> */}
+        <div
+            style={{
+                width: '100%',
+                maxWidth: '1000px',
+                minWidth: '600px',
+                marginBottom: '30px',
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start'
+            }}
+        >
+            <div
+                className="article_info"
+                style={{
+                    width: '80%',
+                    maxHeight: '200px',
+                }}
+            >
+                {/* 아래 부분 '섹션'에 대한 정보가 나와야 하나, 아직 백엔드와 해당 데이터에 대한 고민을 하지 못해 일단 비워놓음  */}
+                {/* <div>{props.section} </div> */}
+                <h2>{title}</h2>
+                <div
+                    className="summary"
+                    style={{
+                        height: '80px',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 4,
+                        WebkitBoxOrient: 'vertical',
+                    }}
+                >
+                    {content}
+                </div>
+                <div className="article_info_footer" style={{ display: 'flex' }}>
+                    <span className="author">기사 작성자 : [{author}]</span>
+                    &nbsp; &nbsp; &nbsp;
+                    {/* todo: 아래 발행일자 부분 일단은 toString() 처리하고 넘어가는데, 나중엔 DateUtil 만들어서 함수 만들어야 함 */}
+                    {/* todo: postman 결과가 publishDate 객체가 아닌 created_at 문자열로 나오는 것 같다 -> 수정해야할 듯 */}
+                    {/* <span className='publish_date'>{publishDate.toString()}</span> */}
+                    <span className="publish_date">{new Date().toString()}</span>
+                </div>
+            </div>
+            <div
+                className="article_thumbnail"
+                style={{
+                    width: '200px',
+                    height: '200px',
+                }}
+            >
+                <img src={thumbnail} alt={title} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            </div>
         </div>
     );
 }
